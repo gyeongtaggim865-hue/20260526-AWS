1d9d79d5a6ec5a2fb27f455a1b8c1b4e93ee4069
gyeongtaggim865-hue <gyeongtaggim865@gmail.com>
2026-05-27 15:01:01 +0900

    work

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,14 +21,13 @@ repositories {
 dependencies {
 	implementation("io.awspring.cloud:spring-cloud-aws-starter-s3:4.0.2")
 
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("tools.jackson.module:jackson-module-kotlin")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 kotlin {
